fad32963ed365df10f4e3883616d2444c8f783f0
Yuriy.Shirokov <Yuriy.Shirokov@05c1cefe-7e4f-0410-8836-23da17caf6b6>
2008-06-19 19:32:55 +0000

    New bag functionality added: now we can drop objects.
    A posibility to save objects in all locations (not a current location only) not implemented yet.

--- engine/scene002.js
+++ engine/scene002.js
@@ -13,7 +13,7 @@ function sceneSetup() {
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
     createDoor(0, 7, 'scene001', 0, 1);
-    var n = createObject(10, 5, 20, 20, 'sc002_obj01.png', 'Crystal Rock');
+    var n = createObject(10, 5, 20, 20, 'sc002_obj01.png', 'Crystal Rock 2');
     if (n != -1) {
         objects[n]['portable'] = true;
         objects[n]['apply'] = applyObjectToObject1;
